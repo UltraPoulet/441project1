b019980cb3ef3728939700d75e1219f00be9ec2f
UltraPoulet <johnrabi@umich.edu>
2013-09-23 15:45:13 -0400

    Commented out createSession stuff
    This is to keep a backup before adding dependencies

--- Project1/src/com/example/project1/CollabrifyAdapterExtended.java
+++ Project1/src/com/example/project1/CollabrifyAdapterExtended.java
@@ -1,10 +1,10 @@
 package com.example.project1;
 
 import android.util.Log;
-import edu.umich.imlc.collabrify.client.CollabrifyAdapter;
+//import edu.umich.imlc.collabrify.client.CollabrifyAdapter;
 
-class CollabrifyAdapterExtended extends CollabrifyAdapter{
+/*class CollabrifyAdapterExtended extends CollabrifyAdapter{
 	public void onSessionCreated(long id){
 		Log.println(1, "I dunno", "Session Created: " + id);
 	}
-}
+}*/
--- Project1/src/com/example/project1/MainActivity.java
+++ Project1/src/com/example/project1/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.project1;
 
-import edu.umich.imlc.collabrify.client.CollabrifyClient;
-import edu.umich.imlc.collabrify.client.exceptions.CollabrifyException;
 import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
@@ -10,53 +8,37 @@ import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
-	public CollabrifyClient newClient;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 	
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item){
+		EditTextModified etm = (EditTextModified)this.getCurrentFocus();
+		switch(item.getItemId()){
+		case R.id.action_settings:
+			return true;
+		case R.id.action_undo:
+			etm.UndoRedoHandler(true);
+			return true;
+		case R.id.action_redo:
+			etm.UndoRedoHandler(false);
+			return true;
+		case R.id.action_createSession:
+			Log.d("Create", "Create Session");
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
-    public boolean onOptionsItemSelected(MenuItem item) {
-    	EditTextModified etm = (EditTextModified)this.getCurrentFocus();
-    	switch (item.getItemId()) {
-	        case R.id.action_settings:
-	        	return true;
-	        case R.id.action_undo:
-	        	etm.UndoRedoHandler(true);
-	        	return true;
-	        case R.id.action_redo:
-	        	etm.UndoRedoHandler(false);
-	        	return true;
-	        case R.id.action_createSession:
-	        	//create session
-	        	try{
-	        		newClient = new CollabrifyClient(this, "user email", "display name", "441fall2013@umich.edu", "XY3721425NoScOpE", false, new CollabrifyAdapterExtended());
-	        	}
-	        	catch( CollabrifyException e ){
-	        		e.printStackTrace();
-	        	}
-	        	try{
-	        		String sessionName = "Rabideau";
-	        		newClient.createSession(sessionName,null,null,2);
-	        		Log.i("Tag", "Session name is " + sessionName);
-	        	}
-	        	catch( CollabrifyException e ){
-	        		Log.e("Tag", "error", e);
-	        	}
-	        	item.setEnabled(false);
-	        	return true;
-	        default:
-	        	return super.onOptionsItemSelected(item);
-    	}
-    }
 }
